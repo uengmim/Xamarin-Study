fa88a06eb1329f8723d48e6892dd558d268b6840
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-04-12 09:10:47 +0900

    데이터모델

---
+++ xamarinStudy/xamarinStudy/Models/NBPSYS/ZnbpRomn.cs
@@ -0,0 +1,158 @@
+// iMATE Auto generator Code
+// (C)Copyright 2022 ISTN
+// RUN : imatecc gen_md -title DS4 -object "ZNBP_ROMN" -output "ZnbpRomn.cs" -nspace "NAMHE.Model" -mtype "proxy" -serial "field"
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+using XNSC;
+using XNSC.DD;
+using XNSC.DD.EX;
+
+namespace NAMHE.Model
+{
+/// <summary>
+    /// ZNBPRomnModel(ZNBP_ROMN) Proxy class
+    /// </summary>
+    public class ZNBPRomnModel
+    {
+        /// <summary>
+        /// MNUID(mnuid) Field
+        /// </summary>
+        public System.String MNUID { get; set; }
+
+        /// <summary>
+        /// APPID(appid) Field
+        /// </summary>
+        public System.String APPID { get; set; }
+
+        /// <summary>
+        /// ROLEID(roleid) Field
+        /// </summary>
+        public System.String ROLEID { get; set; }
+
+        /// <summary>
+        /// UIID(uiid) Field
+        /// </summary>
+        public System.String UIID { get; set; }
+
+        /// <summary>
+        /// MNUTYP(mnutyp) Field
+        /// </summary>
+        public System.String MNUTYP { get; set; }
+
+        /// <summary>
+        /// MNUNM(mnunm) Field
+        /// </summary>
+        public System.String MNUNM { get; set; }
+
+        /// <summary>
+        /// PMNUID(pmnuid) Field
+        /// </summary>
+        public System.String PMNUID { get; set; }
+
+        /// <summary>
+        /// ORGTYP(orgtyp) Field
+        /// </summary>
+        public System.String ORGTYP { get; set; }
+
+        /// <summary>
+        /// ICON(icon) Field
+        /// </summary>
+        public System.String ICON { get; set; }
+
+        /// <summary>
+        /// REFDA1(rEFDA1) Field
+        /// </summary>
+        public System.String REFDA1 { get; set; }
+
+        /// <summary>
+        /// REFDA2(rEFDA2) Field
+        /// </summary>
+        public System.String REFDA2 { get; set; }
+
+        /// <summary>
+        /// REFDA3(rEFDA3) Field
+        /// </summary>
+        public System.String REFDA3 { get; set; }
+
+        /// <summary>
+        /// REFDA4(rEFDA4) Field
+        /// </summary>
+        public System.String REFDA4 { get; set; }
+
+        /// <summary>
+        /// REFDA5(rEFDA5) Field
+        /// </summary>
+        public System.String REFDA5 { get; set; }
+
+        /// <summary>
+        /// REFDT1(rEFDT1) Field
+        /// </summary>
+        public System.DateTime REFDT1 { get; set; }
+
+        /// <summary>
+        /// REFDT2(rEFDT2) Field
+        /// </summary>
+        public System.DateTime REFDT2 { get; set; }
+
+        /// <summary>
+        /// REFDT3(rEFDT3) Field
+        /// </summary>
+        public System.DateTime REFDT3 { get; set; }
+
+        /// <summary>
+        /// CRTUSR(crtusr) Field
+        /// </summary>
+        public System.String CRTUSR { get; set; }
+
+        /// <summary>
+        /// CRTDT(crtdt) Field
+        /// </summary>
+        public System.DateTime CRTDT { get; set; }
+
+        /// <summary>
+        /// CRTTM(crttm) Field
+        /// </summary>
+        public System.TimeSpan CRTTM { get; set; }
+
+        /// <summary>
+        /// UPDUSR(updusr) Field
+        /// </summary>
+        public System.String UPDUSR { get; set; }
+
+        /// <summary>
+        /// UPDTM(updtm) Field
+        /// </summary>
+        public System.TimeSpan UPDTM { get; set; }
+
+        /// <summary>
+        /// UPDDT(upddt) Field
+        /// </summary>
+        public System.DateTime UPDDT { get; set; }
+
+   
+        /// <summary>
+        /// 모델의 상태
+        /// </summary>
+        public DIMModelStatus ModelStatus { get; set; }
+
+    }
+
+/// <summary>
+    /// ZNBPRomnModel(ZNBP_ROMN) Proxy List Class
+    /// </summary>    
+    public class ZNBPRomnModelList  : List<ZNBPRomnModel>
+    {
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public ZNBPRomnModelList()
+        {
+            return;
+        }
+    }
+
+}
