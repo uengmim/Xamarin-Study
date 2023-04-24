de6a0dc16b0a7352eb1b32e42ceda0d7cb3e3b3c
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-04-24 09:15:40 +0900

    MFRM

---
+++ xamarinStudy/xamarinStudy/Pages/MFMMG/Modals/MFRMSub.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Acr.UserDialogs;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+using NMAP.ViewModels.MFMMG;
+using NMAP.ViewModels.Common;
+using NMAP.ViewModels.MFMIN;
+using NMAP.Pages.MFMMG.Modals;
+using NMAP.Utils;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+using NAMHE.Model;
+using XNSC.DD.EX;
+using XNSC.Net;
+using NMAP.Models.Common;
+using System.Collections.ObjectModel;
+using DevExpress.XamarinForms.Editors;
+
+namespace NMAP.Pages.MFMMG
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MFRMSub : ContentPage, INMapAppInit
+    {
+        public MFRMViewModel viewModel { get; set; }
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public MFRMSub(MFRMViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = new MFRMViewModel(this, viewModel);
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        #region INMapAppInfo의 구현
+
+        /// <summary>
+        /// 로그린 정보
+        /// </summary>
+        public LoginDataModel LogInInfo { get; set; }
+
+        /// <summary>
+        /// 메뉴 정보
+        /// </summary>
+        public MenuDataModel MenuInfo { get; set; }
+
+        /// <summary>
+        /// 바인딩 컨텍스를 초기화 한다.
+        /// </summary>
+        public void InitBindingContext()
+        {
+            BindingContext = viewModel = new MFRMViewModel(this, viewModel);
+        }
+
+        #endregion
+        /// <summary>
+        /// 로딩 다이얼로그 온/오프
+        /// </summary>
+        /// <param name="yn"></param>
+        public void OnLoadingDialog(bool yn)
+        {
+            if (yn)
+                UserDialogs.Instance.ShowLoading();
+            else
+                UserDialogs.Instance.HideLoading();
+
+        }
+
+
+        /// <summary>
+        /// 뒤로가기 버튼 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBack_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+
+        /// <summary>
+        /// 상세내역 화면이동 이벤트
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public async void on_datail(MFRMViewModel viewModel)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new MFRMDetail(viewModel)));
+        }
+
+        /// <summary>
+        /// 구매요청 저장 다이얼로그 이벤트
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> OnSaveDialogOpen()
+        {
+            return await DisplayAlert("저장", "해당 내용으로 요청하시겠습니까?", "예", "아니오");
+        }
+        public async void OnResultMessage(string sCode, string sMsg)
+        {
+            if (sCode != "S")
+                await DisplayAlert("결과메세지", sMsg, Properties.Resources.Dialog_Confirm);
+            else
+                await DisplayAlert("성공", "요청성공 (구매요청번호 : "+sMsg +")", Properties.Resources.Dialog_Confirm);
+        }
+
+        public async void OnErrorMessage(string sMsg)
+        {
+                await DisplayAlert("알림", sMsg, Properties.Resources.Dialog_Confirm);
+        }
+
+
+
+    }
+}
