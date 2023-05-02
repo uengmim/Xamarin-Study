ffd6b594ded01b2c41e31c41a0cc4901650c7b01
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-05-02 09:09:17 +0900

    Model

---
+++ xamarinStudy/xamarinStudy/Utils/CircleStateImageConvert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NMAP.Utils
+{
+    internal class CircleStateImageConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string source;
+            switch (value)
+            {
+                case "REL":
+                    source = "Circle_Green_24.png";
+                    break;
+                case "":
+                default:
+                    source = "Circle_Gray_24.png";
+                    break;
+            }
+            return source;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
