959ab1cec7d7acece3e044cdc19ba5fd6c7ae05b
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-05-15 09:58:18 +0900

    selector

---
+++ xamarinStudy/xamarinStudy/Utils/TemplateSelectors.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.XamarinForms.DataGrid;
+using NMAP.ViewModels;
+using NMAP.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NMAP.Utils
+{
+
+    public class MenuListTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate HasSubMenu { get; set; }
+
+        public DataTemplate NoneSubMenu { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            MenuDataModel target = item as MenuDataModel;
+
+            return (target?.Items?.Count ?? 0) > 0 ? HasSubMenu : NoneSubMenu;
+        }
+    }
+}
