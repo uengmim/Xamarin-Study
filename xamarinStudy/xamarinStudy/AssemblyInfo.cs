a1330f7175bf6e6f4f0aef31bd69dbbbe6aec908
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-06-03 18:09:30 +0900

    Assembly

--- xamarinStudy/xamarinStudy/AssemblyInfo.cs
+++ xamarinStudy/xamarinStudy/AssemblyInfo.cs
@@ -1,3 +1,16 @@
+using System.Resources;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: NeutralResourcesLanguage("ko-KR")]
+[assembly: ExportFont("NotoSansKR-Regular.otf", Alias = "NotoSansKR")]
+[assembly: ExportFont("NotoSansKR-Black.otf", Alias = "NotoSansKRBlack")]
+[assembly: ExportFont("NotoSansKR-Light.otf", Alias = "NotoSansKRLight")]
+[assembly: ExportFont("NotoSansKR-Bold.otf", Alias = "NotoSansKRBold")]
+[assembly: ExportFont("NanumGothic-Regular.ttf", Alias = "NanumGothic")]
+[assembly: ExportFont("NanumGothic-Bold.ttf", Alias = "NanumGothicBold")]
+[assembly: ExportFont("PatrickHand-Regular.ttf", Alias = "PatrickHand")]
+[assembly: ExportFont("GlassAntiqua-Regular.ttf", Alias = "GlassAntiqua")]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
