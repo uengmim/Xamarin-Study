0a509a5a71a3d37a19fcadc898061d80ef6a53e3
채승민 <72143238+yalmipzi@users.noreply.github.com>
2023-06-08 13:09:15 +0900

    acti

--- xamarinStudy/xamarinStudy.Android/MainActivity.cs
+++ xamarinStudy/xamarinStudy.Android/MainActivity.cs
@@ -4,20 +4,56 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content.Res;
+using Acr.UserDialogs;
+using System.Reflection.Metadata;
 
-namespace xamarinStudy.Droid
+namespace NMAP.Droid
 {
-    [Activity(Label = "xamarinStudy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Name = "kr.co.nhchem.nmap.MainActivity", Label = "남해화학", Icon = "@mipmap/icon", Exported = true, Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+    Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+    DataScheme = "pf0020a143",
+    DataHost = "login")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override Resources Resources
+        {
+
+            get
+            {
+                Resources resource = base.Resources;
+                Configuration configuration = new Configuration();
+                configuration.SetToDefaults();
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.NMr1)
+                {
+                    return CreateConfigurationContext(configuration).Resources;
+                }
+                else
+                {
+                    return resource;
+                }
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+           base.OnCreate(savedInstanceState);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            UserDialogs.Init(this);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            
             LoadApplication(new App());
+
+            App.ScreenHeight = (int)(Resources.DisplayMetrics.HeightPixels / Resources.DisplayMetrics.Density);
+            App.ScreenWidth = (int)(Resources.DisplayMetrics.WidthPixels / Resources.DisplayMetrics.Density);
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
